31fd90eff095b6f0789d5e4536a3959eae1de425
RivaD2 <rivkadavidowski@gmail.com>
2020-07-07 16:31:44 -0700

    edited CSS and welcome message code in js file

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/*Prompt the user a total of exactly five yes or no questions. The user input for the answer must accept either y/n or yes/no responses while taking into consideration case sensitivity */
+
+//Ask the user their name through a prompt()
+//Display that name back to the user through a custom greeting welcoming them to my site
+//Display the user’s name back to them in your final message to the use
+
+//Add console.log() messages within my site to notify the user if they are correct
+//use .toUpperCase() or .toLowerCase() functions
+//Before submitting, comment out (don’t delete) your console.log()
+
+var welcomeName = prompt('what is your name?');
+alert('Welcome' + welcomeName + 'to my site!');
+console.log('welcomeName' + welcomeName);
+
+
+
